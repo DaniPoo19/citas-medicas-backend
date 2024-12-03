c08fcbe823e78a4bc317a83d3933037d644d9598
DaniPoo19 <vnegretebaza24@correo.unicordoba.edu.co>
2024-12-03 00:44:11 -0500

    Seguridad mejorada variables de entorno

--- config.py
+++ config.py
@@ -1,17 +1,11 @@
-from flask import Flask, request, jsonify
+import os
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_cors import CORS  # Importa CORS
 
-# Crear instancia de la aplicación
 app = Flask(__name__)
 
-# Configuración de la base de datos
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:vQXKrEivnMiXrhWioRJniLfCIXTlwNOS@junction.proxy.rlwy.net:42172/railway'
+# Usar la variable de entorno para la URL de la base de datos
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# Instanciar la base de datos
 db = SQLAlchemy(app)
-
-# Configurar CORS para la aplicación
-CORS(app, resources={r"/*": {"origins": "https://danipoo19.github.io"}})  # Aquí va la configuración de CORS
-
