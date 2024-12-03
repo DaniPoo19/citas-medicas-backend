fa1ee94779e154de26ce63b5565198bc816c8d7f
DaniPoo19 <vnegretebaza24@correo.unicordoba.edu.co>
2024-12-03 00:14:06 -0500

    DataBase Fixed

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from config import app, db
@@ -171,7 +172,6 @@ def obtener_citas():
         return jsonify({'message': str(e)}), 400
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=int(os.getenv("PORT", 5000)))
+
 
--- config.py
+++ config.py
@@ -1,12 +1,17 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from flask_migrate import Migrate
 
+# Inicializar la app y configurar CORS
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/*": {"origins": "*"}})  # Cambiar "*" por un dominio específico en producción
 
-# Configuración de la base de datos
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:vQXKrEivnMiXrhWioRJniLfCIXTlwNOS@junction.proxy.rlwy.net:42172/railway'
+# Configuración de la base de datos usando variables de entorno
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'mysql+pymysql://root:password@localhost/db_name')  # Proporciona un valor por defecto para desarrollo local
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+# Inicializar la base de datos y las migraciones
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
